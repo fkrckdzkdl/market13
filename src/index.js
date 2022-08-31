89aba7d89d43492f3f365e3eb6ee861a49e1311a
moonseongmin97 <wja04097@naver.com>
2022-08-31 20:20:58 +0900

    무야호

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-//test
+//test 종훈이형
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
