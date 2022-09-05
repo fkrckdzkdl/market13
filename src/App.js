87fdcdf66396f2884f8caf94efe9d536b375f0c7
geontaekim <109645318+geontaekim@users.noreply.github.com>
2022-09-05 09:10:26 +0900

    하아....

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
-
 import ItemList from './components/itemList/ItemList';
 import Board from './components/board/Board';
 import Member from './components/member/Member';
